95204f86733d8a386256ed5da732f4bf1d489e84
Nguyễn Hoàng Minh <46775788+MinhWalker@users.noreply.github.com>
2024-04-24 18:01:18 +0700

    Initial commit

---
+++ service/article_services_impl.go
@@ -0,0 +1,134 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/bxcodec/go-clean-arch/domain"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+/*
+* In this function below, I'm using errgroup with the pipeline pattern
+* Look how this works in this package explanation
+* in godoc: https://godoc.org/golang.org/x/sync/errgroup#ex-Group--Pipeline
+ */
+func (a *Service) fillAuthorDetails(ctx context.Context, data []domain.Article) ([]domain.Article, error) {
+	g, ctx := errgroup.WithContext(ctx)
+	// Get the author's id
+	mapAuthors := map[int64]domain.Author{}
+
+	for _, article := range data { //nolint
+		mapAuthors[article.Author.ID] = domain.Author{}
+	}
+	// Using goroutine to fetch the author's detail
+	chanAuthor := make(chan domain.Author)
+	for authorID := range mapAuthors {
+		authorID := authorID
+		g.Go(func() error {
+			res, err := a.authorRepo.GetByID(ctx, authorID)
+			if err != nil {
+				return err
+			}
+			chanAuthor <- res
+			return nil
+		})
+	}
+
+	go func() {
+		defer close(chanAuthor)
+		err := g.Wait()
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+
+	}()
+
+	for author := range chanAuthor {
+		if author != (domain.Author{}) {
+			mapAuthors[author.ID] = author
+		}
+	}
+
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
+
+	// merge the author's data
+	for index, item := range data { //nolint
+		if a, ok := mapAuthors[item.Author.ID]; ok {
+			data[index].Author = a
+		}
+	}
+	return data, nil
+}
+
+func (a *Service) Fetch(ctx context.Context, cursor string, num int64) (res []domain.Article, nextCursor string, err error) {
+	res, nextCursor, err = a.articleRepo.Fetch(ctx, cursor, num)
+	if err != nil {
+		return nil, "", err
+	}
+
+	res, err = a.fillAuthorDetails(ctx, res)
+	if err != nil {
+		nextCursor = ""
+	}
+	return
+}
+
+func (a *Service) GetByID(ctx context.Context, id int64) (res domain.Article, err error) {
+	res, err = a.articleRepo.GetByID(ctx, id)
+	if err != nil {
+		return
+	}
+
+	resAuthor, err := a.authorRepo.GetByID(ctx, res.Author.ID)
+	if err != nil {
+		return domain.Article{}, err
+	}
+	res.Author = resAuthor
+	return
+}
+
+func (a *Service) Update(ctx context.Context, ar *domain.Article) (err error) {
+	ar.UpdatedAt = time.Now()
+	return a.articleRepo.Update(ctx, ar)
+}
+
+func (a *Service) GetByTitle(ctx context.Context, title string) (res domain.Article, err error) {
+	res, err = a.articleRepo.GetByTitle(ctx, title)
+	if err != nil {
+		return
+	}
+
+	resAuthor, err := a.authorRepo.GetByID(ctx, res.Author.ID)
+	if err != nil {
+		return domain.Article{}, err
+	}
+
+	res.Author = resAuthor
+	return
+}
+
+func (a *Service) Store(ctx context.Context, m *domain.Article) (err error) {
+	existedArticle, _ := a.GetByTitle(ctx, m.Title) // ignore if any error
+	if existedArticle != (domain.Article{}) {
+		return domain.ErrConflict
+	}
+
+	err = a.articleRepo.Store(ctx, m)
+	return
+}
+
+func (a *Service) Delete(ctx context.Context, id int64) (err error) {
+	existedArticle, err := a.articleRepo.GetByID(ctx, id)
+	if err != nil {
+		return
+	}
+	if existedArticle == (domain.Article{}) {
+		return domain.ErrNotFound
+	}
+	return a.articleRepo.Delete(ctx, id)
+}
